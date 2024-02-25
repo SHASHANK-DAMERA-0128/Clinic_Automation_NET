95ae69b41b33a890d8866a8003177af6cb70be99
Amit Sahoo <miamitsahoo@gmail.com>
2024-02-25 13:59:30 +0530

    Fix: Create appointment

--- Clinic_Automation/Models/appointmentExtend.cs
+++ Clinic_Automation/Models/appointmentExtend.cs
@@ -10,13 +10,19 @@ namespace Clinic_Automation.Models
 {
     public class appointmentExtend
     {
-       
-        
+
+
+        //[Display(Name = "Appointment Date & Time")]
+        //[Required(AllowEmptyStrings = false, ErrorMessage = "Please Enter the valid Date and Time")]
+        //[DataType(DataType.DateTime)]
+        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-ddTHH:mm:ss}")]
+        //[appdatetimevalidation(ErrorMessage = "Please Enter the valid Date and Time")]
+        //public System.DateTime AppointmentDateTime { get; set; }
         [Display(Name = "Appointment Date & Time")]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Enter the valid Date and Time")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Enter a valid Date and Time")]
         [DataType(DataType.DateTime)]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-ddTHH:mm:ss}")]
-        [appdatetimevalidation(ErrorMessage = "Please Enter the valid Date and Time")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy HH:mm tt}")]
+        [appdatetimevalidation(ErrorMessage = "You have chosen a date in the past. Please select a future date to proceed.")]
         public System.DateTime AppointmentDateTime { get; set; }
         [Display(Name = "Criticality")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please Enter the valid Criticality")]
